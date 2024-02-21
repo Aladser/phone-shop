8577c5f5460e3c4cfe70a6f9126ca812ebf50907
Aladser <Aladser@users.noreply.github.com>
2024-02-21 16:43:05 +0900

    обновлены роуты

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PhoneController::class, 'index'])->name('main');
 Route::get('/dashboard', [OrderController::class, 'create'])->middleware(['auth', 'verified'])->name('dashboard');
+
+// Order
 Route::get('/order', [OrderController::class, 'index'])->name('order.index');
 Route::post('/order', [OrderController::class, 'store'])->name('order.store');
 Route::delete('/order', [OrderController::class, 'destroy'])->name('order.destroy');
 
+// Profile
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('/phone', PhoneController::class)
-    ->except(['show', 'create', 'edit', 'update', 'destroy'])
-    ->middleware(['auth']);
+// Phone
+Route::get('/phone', [PhoneController::class, 'index'])->middleware(['auth'])->name('phone.index');
+Route::post('/phone', [PhoneController::class, 'store'])->middleware(['auth'])->name('phone.store');
 
 require __DIR__.'/auth.php';
