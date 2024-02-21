d05e4ef48660c1319dc4252e745d84d57fdad5a1
Aladser <Aladser@users.noreply.github.com>
2024-02-21 16:38:01 +0900

    тест

--- app/Http/Controllers/BasketPhoneController.php
+++ app/Http/Controllers/BasketPhoneController.php
@@ -3,31 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\BasketPhone;
-use App\Models\Phone;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class PhoneController extends Controller
+class BasketPhoneController extends Controller
 {
     public function index()
     {
-        $authUser = Auth::user();
-        if ($authUser) {
-            $basketPhoneCount = BasketPhone::where('user_id', $authUser->id)->sum('count');
-            $is_auth = true;
-        } else {
-            $basketPhoneCount = 0;
-            $is_auth = false;
-        }
-
-        return view(
-            'welcome',
-            [
-                'phones' => Phone::all(),
-                'is_auth' => $is_auth,
-                'basket_phone_count' => $basketPhoneCount,
-            ]
-        );
+        echo 'test';
     }
 
     public function store(Request $request)
